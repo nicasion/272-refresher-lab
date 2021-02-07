6ab0376cd577a4d42d4eaec00f0018c01cd3b5e0
Nicasio <nicasio@cmu.edu>
2021-02-06 22:50:44 -0500

    added relationship/validation macros to child_test

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class ChildTest < ActiveSupport::TestCase
   # TODO: Relationship macros
-  
+  should have_many(:chores)
+  should have_many(:tasks).through(:chores)
   # TODO: Validation macros
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
 
   # TODO: Context testing
 
