0100a1f1196eabab3118e933e14260d609cf6457
Nicasio <nicasio@cmu.edu>
2021-02-06 23:28:57 -0500

    passed task_test with updates to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,14 @@
 class Task < ApplicationRecord
+    # Relationships
+    has_many :chores
+    has_many :children, through: :chores
+
+    # Validations
+    validates_presence_of :name
+    validates_numericality_of :points, :greater_than_or_equal_to => 0, :only_integer => true
+
+    # Scopes
+    scope :alphabetical, -> { order('name') }
+    scope :active, -> { where(active: true) }
+
 end
