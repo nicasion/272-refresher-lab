6fc69f3e5b6bf76afdcb8b3e1c37ee8b14ace2b7
Nicasio <nicasio@cmu.edu>
2021-02-06 23:48:06 -0500

    all chore_test passed w changes to chore model

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,6 +1,26 @@
 class Chore < ApplicationRecord
-
+    # Relationships
     belongs_to :child
     belongs_to :task
   
+    # Validations
+    validates_date :due_on
+
+
+    # Scopes
+    scope :by_task,  -> { joins(:task).order("name") }
+    scope :chronological, -> { order('due_on','completed') }
+    scope :pending, -> { where(completed: false) }
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> { where("due_on >= ?", Date.current)}
+    scope :past, -> { where("due_on < ?", Date.current)}
+
+    # Other methods
+    def status
+        completion_bool = self.completed
+        return "Completed" if completion_bool == true
+        "Pending"
+    end
+
+
 end
